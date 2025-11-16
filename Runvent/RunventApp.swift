be72c1242e4b39a42acf7894d135063f80df1607
TNxRebel <housemfarhani@gmail.com>
2025-11-16 18:42:13 +0100

    Initial Commit

---
+++ Runvent/RunventApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunventApp.swift
+//  Runvent
+//
+//  Created by Houssem Farhani on 16.11.25.
+//
+
+import SwiftUI
+
+@main
+struct RunventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
